9358ef3a6fc02394107f9aa61a5078ef7f8d78c5
GustavoBozzano <gustavojorgebozzano@gmail.com>
2023-10-06 17:26:24 +0200

    ejercicios resueltos 6-10

--- Semana 4/Ej5 - Bucles mas Complicados/index.js
+++ Semana 4/Ej5 - Bucles mas Complicados/index.js
@@ -9,12 +9,12 @@ let sumaBuho12 = "";
 
 for (i = 0; i <= 23; i++) {
   if (i >= 8 && i <= 12) {
-    console.log("Son las " + i + ":" + minute + minute + "," + sumaBuho24);
+    console.log(`Son las ${i}:${minute}${minute},${sumaBuho24}`);
   } else if (i >= 13 && i <= 22) {
     sumaBuho12 += buho;
-    console.log("Son las " + i + ":" + minute + minute + "," + sumaBuho12);
+    console.log(`Son las ${i}:${minute}${minute},${sumaBuho12}`);
   } else {
-    console.log("Son las " + i + ":" + minute + minute);
+    console.log(`Son las ${i}:${minute}${minute}`);
   }
   sumaBuho24 += buho;
 }
