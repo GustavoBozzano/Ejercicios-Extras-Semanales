f791775e8a633508595d73c2bd08d950903d85aa
GustavoBozzano <gustavojorgebozzano@gmail.com>
2024-03-22 13:55:35 +0100

    Updates

--- Semana 4/Ej5 - Bucles mas Complicados/index.js
+++ Semana 4/Ej5 - Bucles mas Complicados/index.js
@@ -1,20 +1,9 @@
 "use strict";
 
-// Escribe aquí tu código
-let i = 0;
-const minute = 0;
-const buho = " CUCÚ!";
-let sumaBuho24 = "";
-let sumaBuho12 = "";
-
-for (i = 0; i <= 23; i++) {
-  if (i >= 8 && i <= 12) {
-    console.log(`Son las ${i}:${minute}${minute},${sumaBuho24}`);
-  } else if (i >= 13 && i <= 22) {
-    sumaBuho12 += buho;
-    console.log(`Son las ${i}:${minute}${minute},${sumaBuho12}`);
-  } else {
-    console.log(`Son las ${i}:${minute}${minute}`);
+for (let hora = 0; hora < 24; hora++) {
+  let strHora = `Son las ${hora}:00`;
+  if (hora >= 8 && hora <= 22) {
+    strHora = strHora + "." + " CUCÚ!".repeat(hora % 12 === 0 ? 12 : hora % 12);
   }
-  sumaBuho24 += buho;
+  console.log(strHora);
 }
