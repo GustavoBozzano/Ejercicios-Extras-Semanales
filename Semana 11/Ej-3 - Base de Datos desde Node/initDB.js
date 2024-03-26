59e67e9c8b91cc158cd52d473e530af9ebfdb1e5
GustavoBozzano <gustavojorgebozzano@gmail.com>
2024-03-26 10:37:16 +0100

    Update initDB.js

--- Semana 11/Ej-3 - Base de Datos desde Node/initDB.js
+++ Semana 11/Ej-3 - Base de Datos desde Node/initDB.js
@@ -1,39 +1,45 @@
 import getPool from "./getpool.js";
+import "dotenv/config";
+
+const { MYSQL_DATABASE } = process.env;
 
 const initDB = async () => {
   try {
-    let pool = await getPool();
+    const pool = await getPool();
 
     console.log("Elimando base de datos...");
-    await pool.query("DROP DATABASE IF EXISTS bootcamp");
+    await pool.query(`DROP DATABASE IF EXISTS ${MYSQL_DATABASE}`);
 
     console.log("Creando base de datos...");
-    await pool.query("CREATE DATABASE bootcamp");
+    await pool.query(`CREATE DATABASE ${MYSQL_DATABASE}`);
 
-    await pool.query("USE bootcamp");
+    await pool.query(`USE ${MYSQL_DATABASE}`);
 
     console.log("Eliminando tablas...");
-    await pool.query("DROP TABLE IF EXISTS estudiantes, direcciones");
+    await pool.query("DROP TABLE IF EXISTS addresses, students");
 
     console.log("Creando tablas...");
 
     await pool.query(`
-          CREATE TABLE IF NOT EXISTS direcciones (
-              id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-              calle VARCHAR(100) NOT NULL,
-              codigo_postal INT UNSIGNED NOT NULL
-          )
-      `);
+    CREATE TABLE IF NOT EXISTS students (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstName VARCHAR(50) NOT NULL,
+        lastName VARCHAR(100) NOT NULL, 
+        birthDate DATETIME NOT NULL,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+`);
+
     await pool.query(`
-          CREATE TABLE IF NOT EXISTS estudiantes (
-              id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-              nombre VARCHAR(100) NOT NULL,
-              apellido VARCHAR(100) NOT NULL,
-              fecha_nacimiento INT UNSIGNED NOT NULL,
-              id_direccion INT UNSIGNED,
-              FOREIGN KEY (id_direccion) REFERENCES direcciones(id)
-      )
-  `);
+    CREATE TABLE IF NOT EXISTS addresses (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        studentId INT UNSIGNED NOT NULL,
+        street VARCHAR(255) NOT NULL,
+        postalCode VARCHAR(10) NOT NULL,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (studentId) REFERENCES students (id)
+    )
+`);
   } catch (error) {
     console.log(error);
   }
